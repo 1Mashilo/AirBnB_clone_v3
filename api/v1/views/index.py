1a59731958c0c5e7a8e7682017f78c1bb6caa9bf
1Mashilo <christophermashilo@gmail.com>
2024-04-29 10:13:07 -0700

    file permission

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+""" Index """
+
 from flask import jsonify
 import app_views from api.v1.views
 
-@app_views.route('/status')
-def get_status():
-    """Returns a JSON response indicating the API status as 'OK'."""
-    data = {'status': 'OK'}
-    return jsonify(data)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
